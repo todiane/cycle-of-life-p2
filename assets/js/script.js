9f67c20d191c184266ca36e05d1d09f15a86c696
todiane <>
2023-06-11 17:28:58 +0000

    Restart style.js file with titles for code only

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,118 +1,43 @@
-// Script.js Page
+//Script.js File
 
-// Scoring game script
+// Global variables 
 const game = () => {
-  let playerScore = 0;
-  let computerScore = 0;
-  let moves = 0;
+    let playerScore = 0;
+    let computerScore =  0;
+    let movesleft = 0;
+    let start = 0;
 
-  // Function to
-  const playGame = () => {
-    const humanBtn = document.querySelector(".human");
-    const plantBtn = document.querySelector(".plant");
-    const soilBtn = document.querySelector(".soil");
-    const playerOptions = [humanBtn, plantBtn, soilBtn];
-    const computerOptions = ["human", "plant", "soil"];
+ // Function to
+    const playGame = () => {
+        const humanButton = document.getElementsByClassName(".human");
+        const plantButton = document.getElementsByClassName(".plant");
+        const soilButton = document.getElementsByClassName(".soil");
+        const playerOptions = [humanButton, plantButton, soilButton];
+        const computerOptions = ["human", "plant", "soil"];
+    };
 
-    // Function for start of game
-    playerOptions.forEach((option) => {
-      option.addEventListener("click", function () {
-        const movesLeft = document.querySelector(".movesleft");
-        moves++;
-        movesLeft.innerText = `Moves Left: ${10 - moves}`;
+// Hides reload the game button until play begins
 
-        const choiceNumber = Math.floor(Math.random() * 3);
-        const computerChoice = computerOptions[choiceNumber];
+/* Event listeners for the three player choice options
+* Detail of functions to run when clicked
+*/
 
-        // Function to check winner
-        winner(this.innerText, computerChoice);
 
-        // Game Over function after 10 moves
-        if (moves == 10) {
-          gameOver(playerOptions, movesLeft);
-        }
-      });
-    });
-  };
+// Main game function loop 
 
-  // Function to decide winner
-  const winner = (player, computer) => {
-    const result = document.querySelector(".result");
-    const playerScoreBoard = document.querySelector(".p-count");
-    const computerScoreBoard = document.querySelector(".c-count");
-    player = player.toLowerCase();
-    computer = computer.toLowerCase();
-    if (player === computer) {
-      result.textContent = "Tie";
-    } else if (player == "plant") {
-      if (computer == "human") {
-        result.textContent = "Computer Won";
-        computerScore++;
-        computerScoreBoard.textContent = computerScore;
-      } else {
-        result.textContent = "Player Won";
-        playerScore++;
-        playerScoreBoard.textContent = playerScore;
-      }
-    } else if (player == "human") {
-      if (computer == "soil") {
-        result.textContent = "Computer Won";
-        computerScore++;
-        computerScoreBoard.textContent = computerScore;
-      } else {
-        result.textContent = "Player Won";
-        playerScore++;
-        playerScoreBoard.textContent = playerScore;
-      }
-    } else if (player == "soil") {
-      if (computer == "plant") {
-        result.textContent = "Computer Won";
-        computerScore++;
-        computerScoreBoard.textContent = computerScore;
-      } else {
-        result.textContent = "Player Won";
-        playerScore++;
-        playerScoreBoard.textContent = playerScore;
-      }
-    }
-  };
+// Function for start of game
 
-  // Function to run when game is over
-  const gameOver = (playerOptions, movesLeft) => {
-    const chooseMove = document.querySelector(".move");
-    const result = document.querySelector(".result");
-    const reloadBtn = document.querySelector(".reload");
 
-    playerOptions.forEach((option) => {
-      option.style.display = "none";
-    });
 
-    chooseMove.innerText = "Game Over!!";
-    movesLeft.style.display = "none";
+// Main game function loop to decide winner
 
-    if (playerScore > computerScore) {
-      result.style.fontSize = "2rem";
-      result.innerText = "You Won The Game";
-      result.style.color = "#308D46";
-    } else if (playerScore < computerScore) {
-      result.style.fontSize = "2rem";
-      result.innerText = "You Lost The Game";
-      result.style.color = "red";
-    } else {
-      result.style.fontSize = "2rem";
-      result.innerText = "Tie";
-      result.style.color = "grey";
-    }
-    reloadBtn.innerText = "Restart";
-    reloadBtn.style.display = "flex";
-    reloadBtn.addEventListener("click", () => {
-      window.location.reload();
-    });
-  };
 
-  // Calling playGame function inside game
-  playGame();
-};
+
+
+// Function to run when game is over
+
+
+// Calling playGame function inside game
+
 
 // Calling the game function
-game();
