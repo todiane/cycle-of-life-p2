999fc312f2c89790eafd1324b0e17ea462f8a911
todiane <>
2023-06-13 20:24:56 +0000

    Add function loops for counting down nuumber of moves left plus showing winner and reset button

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,6 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
     playerChoiceDisplay.innerHTML = playerChoice;
     generateComputerChoice();
     getResult();
-    movesleft();
 }));
 
 // Function for start of game
@@ -64,7 +63,7 @@ function getResult() {
 }
 
 // Moves Countdown function loop 
-playerOptions.forEach(option => {
+playerChoice.forEach(option => {
     option.addEventListener('click', function () {
 
         const movesLeft = document.querySelector('.movesleft');
@@ -85,6 +84,47 @@ playerOptions.forEach(option => {
     });
 });
 
+// Function to run when game is over
+const gameOver = (playerOptions, movesLeft) => {
+
+    const chooseMove = document.querySelector('.move');
+    const result = document.querySelector('.result');
+    const reloadBtn = document.querySelector('.reload');
+
+    playerOptions.forEach(option => {
+        option.style.display = 'none';
+    });
+
+    chooseMove.innerText = 'Game Over!!';
+    movesLeft.style.display = 'none';
+
+    if (playerScore > computerScore) {
+        result.style.fontSize = '2rem';
+        result.innerText = 'You Won The Game';
+        result.style.color = '#308D46';
+    }
+    else if (playerScore < computerScore) {
+        result.style.fontSize = '2rem';
+        result.innerText = 'You Lost The Game';
+        result.style.color = 'red';
+    }
+    else {
+        result.style.fontSize = '2rem';
+        result.innerText = 'Tie';
+        result.style.color = 'grey';
+    }
+    reloadBtn.innerText = 'Restart';
+    reloadBtn.style.display = 'flex';
+    reloadBtn.addEventListener('click', () => {
+        window.location.reload();
+    });
+};
 
 
-// Hides reload the game button until play begins
+// Calling playGame function inside game
+playGame();
+
+
+// Calling the game function
+game();
+
