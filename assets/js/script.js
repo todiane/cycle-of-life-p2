5c4ccff368d58f8a7089bf2c629b3570c9941ee0
todiane <>
2023-06-27 18:30:29 +0000

    Add winner and loser html pages with styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,128 +1,122 @@
-//Script.js File
+// A scissor, Rock And Paper Game
+// 1 - Soil
+// 2 - Human
+// 3 - Plant
 
-//Global variables
-const playerChoiceDisplay = document.getElementById('player-choice');
-const computerChoiceDisplay = document.getElementById('computer-choice');
-const resultDisplay = document.getElementById('result');
-const possibleChoices = document.querySelectorAll('button');
-let playerChoice;
-let computerChoice;
-let result;
+// 1 - Human
+// 2 - Plant
+// 3 - Soil
 
-/* Event listeners for the three player choice options
-* Detail of functions to run when clicked*/
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    playerChoice = e.target.id;
-    playerChoiceDisplay.innerHTML = playerChoice;
-    generateComputerChoice();
-    getResult();
-}));
+// Variables
+let comChoice = 0;
+let comOutput = "";
+let comWin = "";
+let playWin = "";
+let playWinCheck = false;
+let comWinCheck = false;
+let playerCount = 0;
+let comCount = 0;
+let userInput = "";
 
-// Function for start of game
-function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length);
-    if (randomNumber === 1) {
-        computerChoice = 'human';
+// Variables ref to HTML
+const human = document.getElementById("human");
+const plant = document.getElementById("plant");
+const soil = document.getElementById("soil");
+const userScore = document.getElementById("userScore");
+const comScore = document.getElementById("comScore");
+const gameComment = document.getElementById("commentText");
+const winComment = document.getElementById("winText");
+
+// Event Listeners
+human.addEventListener("click", humanChoice);
+plant.addEventListener("click", plantChoice);
+soil.addEventListener("click", soilChoice);
+
+//Computer Choice
+function computerPlay() {
+    for (let i = 0; i < 1; i++) {
+        comChoice = Math.floor(Math.random() * 3) + 1;
+        if (comChoice === 1) {
+            comOutput = "human";
+        } else if (comChoice === 2) {
+            comOutput = "plant";
+        } else comOutput = "soil";
     }
-    if (randomNumber === 2) {
-        computerChoice = 'soil';
-    }
-    if (randomNumber === 3) {
-        computerChoice = 'plant';
-    }
-    computerChoiceDisplay.innerHTML = computerChoice;
+    return comOutput;
 }
 
-// Main game function to decide winner
-function getResult() {
-    if (computerChoice === playerChoice) {
-        result = 'Its a draw!';
+function playRound(userInput, computerSelection) {
+    // Resetter to default False
+    playWinCheck = false;
+    comWinCheck = false;
+    //The Game Play
+    let playerOut = userInput;
+    let comOut = computerSelection;
+    if (comOut === "soil" && playerOut === "human") {
+        playWin = "You Lose! Soil Decomposses Human";
+        playWinCheck = true;
+        return playWin;
+    } else if (comOut === "soil" && playerOut === "plant") {
+        comWin = "You Win! Plant Grows In Soil";
+        comWinCheck = true;
+        return comWin;
+    } else if (comOut === "human" && playerOut === "plant") {
+        playWin = "You Lose! Human eats Plant";
+        playWinCheck = true;
+        return playWin;
+    } else if (comOut === "human" && playerOut === "soil") {
+        comWin = "You Win! Soil Decomposses Human";
+        comWinCheck = true;
+        return comWin;
+    } else if (comOut === "plant" && playerOut === "human") {
+        comWin = "You Win! Human Eats Plant";
+        comWinCheck = true;
+        return comWin;
+    } else if (comOut === "plant" && playerOut === "soil") {
+        playWin = "You Lose! Plant Beats Soil";
+        playWinCheck = true;
+        return playWin;
+    } else if (comOut === playerOut) {
+        let bothEqual = "It's a tie";
+        return bothEqual; //When equal
     }
-    if (computerChoice === 'human' && playerChoice === "plant") {
-        result = 'You lose!';
-    }
-    if (computerChoice === 'human' && playerChoice === "soil") {
-        result = 'You win!';
-    }
+}
+// Input Level
+function game(userInput) {
+    if (playerCount !== 7 && comCount !== 7) {
+        let computerSelection = computerPlay();
+        let finaleOutput = playRound(userInput, computerSelection);
+        commentText.innerText = finaleOutput;
+        if (playWinCheck === true) playerCount++;
+        else if (comWinCheck === true) comCount++;
 
-    if (computerChoice === 'plant' && playerChoice === "human") {
-        result = 'You win!';
+        userScore.innerText = playerCount;
+        comScore.innerText = comCount;
+
+        // Final Output
+        setTimeout(() => {
+            if (playerCount === 7 || comCount === 7) {
+                if (playerCount > comCount) {
+                    setTimeout(() => {
+                        window.location.href = "winner.html";
+                    }, 2000);
+                } else {
+                    setTimeout(() => {
+                        window.location.href = "loser.html";
+                    }, 2000);
+                }
+            }
+        }, 0);
     }
-    if (computerChoice === 'plant' && playerChoice === "soil") {
-        result = 'You lose!';
-    }
-    if (computerChoice === 'soil' && playerChoice === "plant") {
-        result = 'You win!';
-    }
-    if (computerChoice === 'soil' && playerChoice === "human") {
-        result = 'You lose!';
-    }
-    resultDisplay.innerHTML = result;
 }
 
-// Moves Countdown function loop 
-playerChoice.forEach(option => {
-    option.addEventListener('click', function () {
-
-        const movesLeft = document.querySelector('.movesleft');
-        moves++;
-        movesLeft.innerText = `Moves Left: ${10 - moves}`;
-
-
-        const choiceNumber = Math.floor(Math.random() * 3);
-        const computerChoice = computerOptions[choiceNumber];
-
-        // Function to check who wins
-        winner(this.innerText, computerChoice);
-
-        // Calling gameOver function after 10 moves
-        if (moves == 10) {
-            gameOver(playerOptions, movesLeft);
-        }
-    });
-});
-
-// Function to run when game is over
-const gameOver = (playerOptions, movesLeft) => {
-
-    const chooseMove = document.querySelector('.move');
-    const result = document.querySelector('.result');
-    const reloadBtn = document.querySelector('.reload');
-
-    playerOptions.forEach(option => {
-        option.style.display = 'none';
-    });
-
-    chooseMove.innerText = 'Game Over!!';
-    movesLeft.style.display = 'none';
-
-    if (playerScore > computerScore) {
-        result.style.fontSize = '2rem';
-        result.innerText = 'You Won The Game';
-        result.style.color = '#308D46';
-    }
-    else if (playerScore < computerScore) {
-        result.style.fontSize = '2rem';
-        result.innerText = 'You Lost The Game';
-        result.style.color = 'red';
-    }
-    else {
-        result.style.fontSize = '2rem';
-        result.innerText = 'Tie';
-        result.style.color = 'grey';
-    }
-    reloadBtn.innerText = 'Restart';
-    reloadBtn.style.display = 'flex';
-    reloadBtn.addEventListener('click', () => {
-        window.location.reload();
-    });
-};
-
-
-// Calling playGame function inside game
-playGame();
-
-
-// Calling the game function
-game();
-
+// Event Lisener Function
+function humanChoice() {
+    game("human");
+}
+function plantChoice() {
+    game("plant");
+}
+function soilChoice() {
+    game("soil");
+}
